5d7aaae65908795de6c4a300151bdecc3cea9ee0
Sébastien Braun <sebbraun@gmail.com>
2018-08-09 15:49:54 +0200

    syntax error patched

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "gce-lb-http" {
-  source  = ""replicated.yet.org/yet/lb-http/google
+  source  = "replicated.yet.org/yet/lb-http/google"
   version = "1.0.8"
   name        = "group-http-lb"
 
